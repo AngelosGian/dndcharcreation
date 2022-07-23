db1dbb9e16487e2a127556a071a1114bde52ab2f
agian <angelos.giannourakos@gmail.com>
2022-07-23 22:03:17 +0300

    .

--- public/main.js
+++ public/main.js
@@ -46,3 +46,26 @@ fetch(`${url}/classes/`, {method: 'GET'})
         })
     })    
 
+fetch(`${url}/alignments/`, {method: 'GET'})
+    .then( results => results.json())
+    .then( data => {
+        console.log(data);
+        const alignments = document.querySelector('#alignments');
+        data.results.map(elem => {
+            console.log(elem.name)
+            const option =document.createElement('option');
+            // console.log(typeof elem.name )
+            // races.appendChild(option).innerText = elem.title
+            option.text = elem.name
+            option.value= elem.index
+            alignments.appendChild(option)
+            console.log(alignments.appendChild(option).value)
+            // I need to add the specific characteristics of the classes
+            // so fetch from url/classes/class 
+            // fetch(`${url}/classes/${option.value}`, {method: 'GET'})
+            // when I choose a class and it will brink me those characteristics
+        
+        })
+    })    
+
+console.log(document.querySelector('#alignments'))
