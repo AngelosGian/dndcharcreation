9cdc3a71810a17fdd25ca0e6ad460e0140dfd4b4
agian <angelos.giannourakos@gmail.com>
2022-07-27 14:38:22 +0300

    trying to add specific functionality

--- public/main.js
+++ public/main.js
@@ -59,17 +59,11 @@ fetch(`${url}/alignments/`, {method: 'GET'})
             option.text = elem.name
             option.value= elem.index
             alignments.appendChild(option)
-            // console.log(alignments.appendChild(option).value)
             // I need to add the specific characteristics of the classes
             // so fetch from url/classes/class 
             // fetch(`${url}/classes/${option.value}`, {method: 'GET'})
             // when I choose a class and it will brink me those characteristics
-            if(alignments.appendChild(option)){
-
-                alignments.ariaSelected = true;
-                console.log(alignments.appendChild(option).value);
-            }
-            
+                        
         })
     })    
     let btn = document.querySelector('#data-send');
