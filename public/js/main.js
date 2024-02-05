4d6aeacc9d5d139a057956c36b6b7a25ae0161cf
Angelos Giannourakos <angelos.giannourakos@gmail.com>
2024-02-05 19:58:22 +0200

    adding dnd features

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const url = 'https://www.dnd5eapi.co/api'
 const races = document.querySelector('#races');
 const classes = document.querySelector('#classes');
 const alignments = document.querySelector('#alignments');
+const features = document.querySelector('#alignments');
 
 fetch(`${url}/races/`, {method: 'GET'})
     .then(result => result.json())
@@ -48,6 +49,26 @@ fetch(`${url}/classes/`, {method: 'GET'})
     })    
 
 fetch(`${url}/alignments/`, {method: 'GET'})
+    .then( results => results.json())
+    .then( data => {
+        // console.log(data);
+      
+        data.results.map(elem => {
+            // console.log(elem.name)
+            const option =document.createElement('option');
+            // console.log(typeof elem.name )
+            // races.appendChild(option).innerText = elem.title
+            option.text = elem.name
+            option.value= elem.index
+            alignments.appendChild(option)
+            // I need to add the specific characteristics of the classes
+            // so fetch from url/classes/class 
+            // fetch(`${url}/classes/${option.value}`, {method: 'GET'})
+            // when I choose a class and it will brink me those characteristics
+                        
+        })
+    })    
+fetch(`${url}/features/`, {method: 'GET'})
     .then( results => results.json())
     .then( data => {
         // console.log(data);
